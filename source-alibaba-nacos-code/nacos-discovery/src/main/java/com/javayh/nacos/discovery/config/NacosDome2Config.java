b98a765a8ca916c20516c76de659781ec86912cf
haiji.yang <haiji.yang@capgemini.com>
2021-02-03 22:08:57 +0800

    add DiscoveryApp :new:

---
+++ source-alibaba-nacos-code/nacos-discovery/src/main/java/com/javayh/nacos/discovery/config/NacosDome2Config.java
@@ -0,0 +1,18 @@
+package com.javayh.nacos.discovery.config;
+
+import lombok.Data;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Dylan
+ * @version 1.0.0
+ * @since 2021-02-01
+ */
+@Data
+public class NacosDome2Config {
+    private String app03;
+    private Boolean status;
+}
