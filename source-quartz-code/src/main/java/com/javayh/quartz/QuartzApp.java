60bc2a90fd4b1f925a54dc8160e777ac0830fa89
haiji.yang <haiji.yang@capgemini.com>
2021-02-09 13:44:13 +0800

    add quartz:heavy_plus_sign: :new:

---
+++ source-quartz-code/src/main/java/com/javayh/quartz/QuartzApp.java
@@ -0,0 +1,20 @@
+package com.javayh.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *      quartz 启动类
+ * </p>
+ *
+ * @author Dylan
+ * @version 1.0.0
+ * @since 2021-02-09
+ */
+@SpringBootApplication
+public class QuartzApp {
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzApp.class,args);
+    }
+}
