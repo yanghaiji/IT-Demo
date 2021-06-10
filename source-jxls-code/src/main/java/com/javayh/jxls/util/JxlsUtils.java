ab7f693df18b816a92ebbe953b55c42c79545440
haiji.yang <haiji.yang@capgemini.com>
2021-06-10 10:49:22 +0800

    add jxls if demo :heavy_plus_sign: :new:

--- source-jxls-code/src/main/java/com/javayh/jxls/util/JxlsUtils.java
+++ source-jxls-code/src/main/java/com/javayh/jxls/util/JxlsUtils.java
@@ -1,9 +1,12 @@
 package com.javayh.jxls.util;
 
+import com.javayh.jxls.model.Department;
+import com.javayh.jxls.model.Employee;
 import com.javayh.jxls.model.UserInfo;
 import org.jxls.common.Context;
 import org.jxls.expression.JexlExpressionEvaluator;
 import org.jxls.transform.Transformer;
+import org.jxls.transform.poi.PoiTransformer;
 import org.jxls.util.JxlsHelper;
 
 import java.io.*;
@@ -20,20 +23,21 @@ import java.util.*;
  * @version 1.0.0
  * @since 2021-06-04
  */
-public class JxlsUtils{
+public class JxlsUtils {
 
     private static List<UserInfo> generateSampleEmployeeData() throws ParseException {
         List<UserInfo> userInfos = new ArrayList<UserInfo>();
-        userInfos.add( new UserInfo(1, "12fawywhagwfqd", "1500", 15) );
-        userInfos.add( new UserInfo(2, "12fawywhagwfqd", "2300", 25) );
-        userInfos.add( new UserInfo(3, "12fawywhagwfqd", "2500", 10) );
-        userInfos.add( new UserInfo(4, "12fawywhagwfqd", "1700", 15) );
-        userInfos.add( new UserInfo(5, "12fawywhagwfqd", "2800", 20) );
+        userInfos.add(new UserInfo(1, "12fawywhagwfqd", "1500", 15));
+        userInfos.add(new UserInfo(2, "12fawywhagwfqd", "2300", 25));
+        userInfos.add(new UserInfo(3, "12fawywhagwfqd", "2500", 10));
+        userInfos.add(new UserInfo(4, "12fawywhagwfqd", "1700", 15));
+        userInfos.add(new UserInfo(5, "12fawywhagwfqd", "2800", 20));
         return userInfos;
     }
 
     /**
      * 简单的excel导出
+     *
      * @throws ParseException
      * @throws IOException
      */
@@ -61,7 +65,61 @@ public class JxlsUtils{
         JxlsHelper.getInstance().setUseFastFormulaProcessor(true)
                 .processTemplate(is, os, context);
         os.close();
+        is.close();
     }
 
 
+
+    private static List<Department> createDepartments() {
+        List<Department> departments = new ArrayList<>();
+        Department department = new Department();
+        department.setLink("http://github.com");
+        department.setName("Derek");
+        department.setChief(new Employee("Git", 28, 2500.00, 2300.00, new Date()));
+        List<Employee> staff = new ArrayList<>();
+        staff.add((new Employee("Git", 28, 1500.00, 1300.00, new Date())));
+        staff.add((new Employee("GITHUB", 28, 3500.00, 1500.00, new Date())));
+        staff.add((new Employee("Yanghaiji", 28, 2500.00, 2300.00, new Date())));
+        staff.add((new Employee("Dylan", 28, 2500.00, 2500.00, new Date())));
+        department.setStaff(staff);
+        departments.add(department);
+
+        Department department1 = new Department();
+        department1.setLink("http://github.com");
+        department1.setName("GIT");
+        department1.setChief(new Employee("Git", 28, 2500.00, 2300.00, new Date()));
+        List<Employee> staff1 = new ArrayList<>();
+        staff1.add((new Employee("Git", 28, 1500.00, 1300.00, new Date())));
+        staff1.add((new Employee("GITHUB", 28, 3500.00, 1500.00, new Date())));
+        staff1.add((new Employee("Yanghaiji", 28, 2500.00, 2300.00, new Date())));
+        staff1.add((new Employee("Dylan", 28, 2500.00, 2500.00, new Date())));
+        department1.setStaff(staff1);
+        departments.add(department1);
+
+        Department department2 = new Department();
+        department2.setLink("http://github.com");
+        department2.setName("HUB");
+        department2.setChief(new Employee("Git", 28, 2500.00, 2300.00, new Date()));
+        List<Employee> staff2 = new ArrayList<>();
+        staff2.add((new Employee("Git", 28, 1500.00, 1300.00, new Date())));
+        staff2.add((new Employee("GITHUB", 28, 3500.00, 1500.00, new Date())));
+        staff2.add((new Employee("Yanghaiji", 28, 2500.00, 2300.00, new Date())));
+        staff2.add((new Employee("Dylan", 28, 2500.00, 2500.00, new Date())));
+        department2.setStaff(staff2);
+        departments.add(department2);
+        return departments;
+    }
+
+    public static void outIf() throws ParseException, IOException {
+        List<UserInfo> userInfos = generateSampleEmployeeData();//group_example_collection
+        OutputStream os =
+                new FileOutputStream("C:\\Dylan\\javayh-demo\\source-jxls-code\\src\\main\\resources\\template\\if_collection.xlsx");
+        InputStream is =
+                new FileInputStream("C:\\Dylan\\javayh-demo\\source-jxls-code\\src\\main\\resources\\template\\if_collection_template.xlsx");
+        Context context = PoiTransformer.createInitialContext();
+        context.putVar("userInfos", userInfos);
+        JxlsHelper.getInstance().processTemplate(is, os, context);
+        os.close();
+        is.close();
+    }
 }
