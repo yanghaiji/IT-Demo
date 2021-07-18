d07253012c2b006636653755cfddd28d2c112706
haiji.yang <haiji.yang@capgemini.com>
2021-07-18 09:47:15 +0800

    add SpEl表达式 :heavy_plus_sign: :new:

---
+++ spring-spel-demo/src/main/java/com/java/spel/SpElApp.java
@@ -0,0 +1,20 @@
+package com.java.spel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Dylan
+ * @version 1.0.0
+ * @since 2021-07-16
+ */
+@SpringBootApplication
+public class SpElApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpElApp.class,args);
+    }
+}
