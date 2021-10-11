ea04022053f46a11ee20b9054d91eb20b3a68804
haiji.yang <haiji.yang@capgemini.com>
2021-10-11 15:18:17 +0800

    add sys oshi demo :heavy_plus_sign: :new:

---
+++ oshi-demo/src/main/java/com/javayh/system/oshi/entity/Sys.java
@@ -0,0 +1,35 @@
+package com.javayh.system.oshi.entity;
+
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class Sys implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 服务器名称
+     */
+    private String computerName;
+
+    /**
+     * 服务器Ip
+     */
+    private String computerIp;
+
+    /**
+     * 项目路径
+     */
+    private String userDir;
+
+    /**
+     * 操作系统
+     */
+    private String osName;
+
+    /**
+     * 系统架构
+     */
+    private String osArch;
+}
