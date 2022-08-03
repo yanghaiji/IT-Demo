f3054cf43349c65db9073ecb54d707644a8ee40e
yanghaiji <372787553@qq.com>
2022-08-03 09:08:50 +0800

    add FlinkCDC :new:

---
+++ flink-demo/src/main/java/com/javayh/advanced/flink/cdc/FlinkCDC.java
@@ -0,0 +1,58 @@
+package com.javayh.advanced.flink.cdc;
+
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2022-08-01
+ */
+public class FlinkCDC {
+    public static void main(String[] args) throws Exception {
+        //1.获取执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //1.1 开启CK并指定状态后端为FS    memory  fs  rocksdb
+        env.setStateBackend(new FsStateBackend("hdfs://localhost:8020/yh-flink/ck"));
+        env.enableCheckpointing(5000L);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(10000L);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000);
+
+        //env.setRestartStrategy(RestartStrategies.fixedDelayRestart());
+
+        //2.通过FlinkCDC构建SourceFunction并读取数据
+        DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+                .hostname("localhost")
+                .port(3306)
+                .username("root")
+                .password("root")
+                .databaseList("yh-flink")
+                //如果不添加该参数,则消费指定数据库中所有表的数据.如果指定,指定方式为db.table
+                .tableList("yh-flink.z_user_info")
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+        DataStreamSource<String> streamSource = env.addSource(sourceFunction);
+
+        //3.打印数据
+        streamSource.print();
+
+        //4.启动任务
+        env.execute("FlinkCDC");
+
+    }
+}
