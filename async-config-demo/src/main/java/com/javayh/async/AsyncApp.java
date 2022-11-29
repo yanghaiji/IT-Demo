87865b921d844006e0667f612dbbcd7807c0f29d
yanghaiji <372787553@qq.com>
2022-11-29 13:56:18 +0800

    add AsyncApp source :new:

---
+++ async-config-demo/src/main/java/com/javayh/async/AsyncApp.java
@@ -0,0 +1,20 @@
+package com.javayh.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2022-11-29
+ */
+@SpringBootApplication
+public class AsyncApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApp.class,args);
+    }
+}
---
+++ async-config-demo/src/main/java/com/javayh/async/config/ExecutorConfig.java
@@ -0,0 +1,46 @@
+package com.javayh.async.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2022-11-29
+ */
+@Slf4j
+@Configuration
+@EnableAsync
+public class ExecutorConfig {
+    private static String namePrefix = "async-javayh";
+
+    @Bean(name = "asyncExecutor01")
+    public Executor asyncExamExecutor() {
+        log.info("start asyncExamExecutor");
+        //计算计算机的核数
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int processors = Runtime.getRuntime().availableProcessors();
+        //配置核心线程数
+        executor.setCorePoolSize(processors * 5);
+        //配置最大线程数
+        executor.setMaxPoolSize(processors * 5);
+        //配置队列大小
+        executor.setQueueCapacity(1000);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix(namePrefix);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+}
