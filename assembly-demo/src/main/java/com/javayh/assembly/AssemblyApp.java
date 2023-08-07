086d0a267c74a642388b90265a6710486625a64a
yanghaiji <372787553@qq.com>
2023-08-07 13:30:43 +0800

    add maven assembly plugin

---
+++ assembly-demo/src/main/java/com/javayh/assembly/AssemblyApp.java
@@ -0,0 +1,20 @@
+package com.javayh.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2023-08-03
+ */
+@SpringBootApplication
+public class AssemblyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AssemblyApp.class, args);
+    }
+}
